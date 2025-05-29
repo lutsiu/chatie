d6d8416adb59d7d4ff06add77245bfdd9bc1eebb
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2025-05-29 14:58:52 +0200

    MethodArgumentNotValidException is not also shown as error in error response for user

--- Chatie/src/main/java/com/example/chatie/Chatie/exception/GlobalExceptionHandler.java
+++ Chatie/src/main/java/com/example/chatie/Chatie/exception/GlobalExceptionHandler.java
@@ -2,13 +2,18 @@ package com.example.chatie.Chatie.exception;
 import com.example.chatie.Chatie.exception.user.DuplicateEmailException;
 import com.example.chatie.Chatie.exception.user.DuplicateUsernameException;
 import com.example.chatie.Chatie.exception.user.UserNotFoundException;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -30,6 +35,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpected(Exception ex) {
         return buildErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+        Map<String, Object> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("message", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(String message, HttpStatus status) {
